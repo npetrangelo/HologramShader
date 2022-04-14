4831b2e17b11486842757b6006dbff0f27b97f0b
Nathan Petrangelo <nathan@petrangelo.org>
2022-04-14 15:58:52 -0400

    Create App project in Xcode

---
+++ HologramShader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HologramShader
+//
+//  Created by qe on 4/14/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
