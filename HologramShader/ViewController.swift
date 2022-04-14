e9805b421dcdee6e5f0567199a9c9f31f51ebd8e
Nathan Petrangelo <nathan@petrangelo.org>
2022-04-14 16:17:47 -0400

    Added mtkView and getMTLDevice()

--- HologramShader/ViewController.swift
+++ HologramShader/ViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import Cocoa
+import MetalKit
 
 class ViewController: NSViewController {
+    var mtkView: MTKView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        mtkView = MTKView()
+        mtkView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mtkView)
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "|[mtkView]|", options: [], metrics: nil, views: ["mtkView" : mtkView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[mtkView]|", options: [], metrics: nil, views: ["mtkView" : mtkView]))
+        
+        let device = getMTLDevice()
+        print(device.name)
+        mtkView.device = device
+        mtkView.colorPixelFormat = .bgra8Unorm
     }
 
     override var representedObject: Any? {
@@ -21,6 +33,15 @@ class ViewController: NSViewController {
         }
     }
 
-
+    func getMTLDevice() -> MTLDevice {
+        let devices = MTLCopyAllDevices()
+        for device in devices {
+            if device.isRemovable {
+                return device
+            }
+        }
+        
+        return MTLCreateSystemDefaultDevice()!
+    }
 }
 
